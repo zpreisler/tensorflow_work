fe30b85e16dda23d5b86e5f7f6b5921a4953a768
Zdenek Preisler <z.preisler@gmail.com>
2018-08-09 20:26:32 +0000

    simple2

--- ann/simple2.py
+++ ann/simple2.py
@@ -21,15 +21,26 @@ class landscape:
 
     def y(self,x,s=0.0):  
         from numpy.random import uniform,normal
+        from numpy import array
         y=0
         for h,mu,sigma in self.v:
             y+=h*self.gaussian(x,mu,sigma)
         noise=normal(0.0,s,len(y))
-        return y/self.n+noise
+        a=y/self.n+noise
+        return array(a,dtype='float32')
 
     def gaussian(self,x,mu,sigma):
         from numpy import exp,sqrt,pi
         return 1.0/sqrt(2*pi*sigma**2)*exp(-(x-mu)**2/(2.0*sigma**2))
+
+class data:
+    def __init__(self,x):
+        self.x=x
+
+    def add_column(self,v):
+        from numpy import full,column_stack
+        a=full(v,len(self.x))
+        self.x2=column_stack((self.x,a))
 
 def main():
     print("linear fit")
@@ -38,19 +49,25 @@ def main():
     from tensorflow.python.ops import resources
 
     from numpy.random import uniform
-    from numpy import arange,linspace,array,sqrt,reshape,log,cast
+    from numpy import arange,linspace,array,sqrt,reshape,log,cast,full,column_stack
     from matplotlib.pyplot import figure,plot,show,xlabel,ylabel,legend,savefig,subplots_adjust
 
-    rr=landscape()
+    rr1=landscape()
+    rr2=landscape()
 
-    x=uniform(-1,2,20000)
-    y=rr.y(x,0.333)
-    #y=rr.y(x,0.01)
+    d=20000
 
-    x=array(x,dtype='float32')
-    y=array(y,dtype='float32')
+    x=uniform(-1,2,d)
+    x_=full(d,1.0)
 
-    feed_x=reshape(x,(-1,1))
+    y=rr1.y(x,0.333)
+
+    x_stacked=column_stack((x,x_))
+
+    x_stacked=array(x_stacked,dtype='float32')
+    #y=array(y,dtype='float32')
+
+    feed_x=reshape(x_stacked,(-1,2))
     feed_y=reshape(y,(-1,1))
 
     dataset=tf.data.Dataset.from_tensor_slices((feed_x,feed_y))
@@ -58,14 +75,20 @@ def main():
     train_dataset=dataset.repeat().batch(4000)
 
     d=1000
-    xo=linspace(-1,2,d)
-    yo=rr.y(xo)
+    xo1=linspace(-1,2,d)
+    xo1_=full(d,1.0)
 
-    xo=array(xo,dtype='float32')
-    yo=array(yo,dtype='float32')
+    yo1=rr1.y(xo1)
 
-    feed_x_true=reshape(xo,(-1,1))
-    feed_y_true=reshape(yo,(-1,1))
+    xo1_stacked=column_stack((xo1,xo1_))
+
+    xo1=array(xo1,dtype='float32')
+    xo1_stacked=array(xo1_stacked,dtype='float32')
+
+    yo1=array(yo1,dtype='float32')
+
+    feed_x_true=reshape(xo1_stacked,(-1,2))
+    feed_y_true=reshape(yo1,(-1,1))
 
     true_dataset=tf.data.Dataset.from_tensor_slices((feed_x_true,feed_y_true))
     validate_dataset=true_dataset.repeat().batch(d)
@@ -119,13 +142,13 @@ def main():
 
     figure()
     plot(x,y,".",alpha=0.005)
-    plot(xo,yo,"-",alpha=0.5,label=r"true")
-    plot(xo,y_predicted,"--",alpha=0.5,label="predicted")
+    plot(xo1,yo1,"-",alpha=0.5,label=r"true")
+    plot(xo1,y_predicted,"--",alpha=0.5,label="predicted")
     xlabel(r"$x$")
     ylabel(r"$y$")
     legend(frameon=False,loc='best')
     subplots_adjust(bottom=0.2,left=0.2)
-    savefig("_simple.png")
+    savefig("_simple2.png")
 
     show()
 
