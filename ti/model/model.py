d88fbd33034f0475669daaa4decfd66499ca6164
Zdenek Preisler <z.preisler@gmail.com>
2018-10-11 17:37:15 -0700

    model

---
+++ ti/model/model.py
@@ -0,0 +1,58 @@
+class network:
+    def __init__(self,inputs,name='network'):
+        print("Initialize %s"%name)
+        self.build_graph(inputs,name=name)
+
+    def build_graph(self,inputs,name='network'):
+        import tensorflow as tf
+        with tf.variable_scope(name):
+            self.dense_1=tf.layers.dense(inputs=inputs,
+                    units=5,
+                    activation=tf.nn.tanh,
+                    name='d1')
+
+            self.dense_2=tf.layers.dense(inputs=self.dense_1,
+                    units=5,
+                    activation=tf.nn.tanh,
+                    name='d2')
+
+            self.dense_3=tf.layers.dense(inputs=self.dense_2,
+                    units=1,
+                    activation=tf.nn.tanh,
+                    name='output_layer')
+
+            self.output_layer=self.dense_3
+
+from myutils import configuration,data
+class data_feeder(configuration):
+    def __init__(self,files,add_data=[],delimiter=[':']):
+        from glob import glob
+        print("Data Feeder")
+        files=glob(files)
+        configuration.__init__(self,files,delimiter=delimiter,add_data=add_data)
+
+        self.dsort(key=lambda x: float(*x['epsilon']))
+
+    def __is_data__(self,name):
+        return any([isinstance(x[name],data) for x in self.dconf])
+
+    def get(self,name):
+        if self.__is_data__(name):
+            return [x[name].data.mean() for x in self.dconf]
+        else:
+            return [float(*x[name]) for x in self.dconf]
+
+    def feed(self,names=[]):
+        from numpy import array
+        d=[]
+        for x in self.dconf:
+            v=[x[name] for name in names]
+            dd=[]
+            for t in v:
+                if isinstance(t,data):
+                    dd+=[t.data.mean()]
+                else:
+                    dd+=[float(*t)]
+            d+=[dd]
+        return array(d)
+
---
+++ ti/ti.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
+def feeder(d):
+    from numpy import array
+    dd=[]
+    for a in d:
+        _eps=float(*a['epsilon'])
+        for _rho in a['.rho'].data:
+            dd+=[[_eps,_rho]]
+    return array(dd)
+
+def feeder2(d):
+    from numpy import array
+    dd=[]
+    for a in d:
+        _eps=float(*a['epsilon'])
+        _rho=a['.rho'].data.mean()
+        dd+=[[_eps,_rho]]
+    return array(dd)
+
+def main(argv):
+    from glob import glob
+    from myutils import configuration
+    from pprint import pprint
+    from matplotlib.pyplot import figure,show,plot,xlabel,ylabel
+    from numpy import array,linspace
+    from model.model import network,data_feeder
+    print("Reading configurations")
+
+    c=data_feeder('eos/fluid*.conf',add_data=['.en','.rho'])
+
+    d=c.dconf
+
+    rho=c.get('.rho')
+    epsilon=c.get('epsilon')
+
+    a=c.feed(['epsilon','.rho','.en'])
+    print(a)
+
+    dd=feeder2(d)
+    fd=dd.transpose()
+
+    print("dd",dd.shape)
+    print(dd[:,0])
+    e=fd[0]
+    e=e.reshape(1,-1)
+    print(e.shape)
+
+    q=[]
+    for e in fd[0]:
+        q+=[[e]]
+    q=array(q)
+
+    w=[]
+    for e in fd[1]:
+        w+=[[e]]
+    w=array(w)
+
+    #print("q:",q,q.shape)
+
+    t=linspace(1,8,2048)
+    t=t.reshape(2048,1)
+    #print("t:",t,t.shape)
+
+    dataset=tf.data.Dataset.from_tensor_slices({'a': q,'b': w})
+    vdataset=tf.data.Dataset.from_tensor_slices({'a': t, 'b': t})
+
+    train_dataset=dataset.repeat().shuffle(len(q)).batch(256)
+    v_dataset=vdataset.batch(2048)
+
+    iterator=tf.data.Iterator.from_structure(train_dataset.output_types,
+            train_dataset.output_shapes)
+    next_element=iterator.get_next()
+
+    init_op=iterator.make_initializer(train_dataset)
+    v_init_op=iterator.make_initializer(v_dataset)
+
+    input_layer=next_element['a']
+    output_layer=next_element['b']
+
+    nn=network(input_layer,name='t')
+    out_rho=nn.output_layer
+
+
+    rate=tf.placeholder(tf.float64)
+
+    loss=tf.reduce_mean(tf.nn.l2_loss((out_rho-output_layer)))
+    optimizer=tf.train.AdamOptimizer(learning_rate=rate,name="Adam")
+
+    train=optimizer.minimize(loss)
+    init_vars=tf.group(tf.global_variables_initializer())
+
+    saver=tf.train.Saver()
+
+    with tf.Session() as session: 
+        session.run(init_vars)
+        session.run(init_op)
+
+        try:
+            saver.restore(session,"log/last.ckpt")
+        except tf.errors.NotFoundError:
+            pass
+
+        for i in range(5):
+            l,_=session.run([loss,train],feed_dict={rate: 1e-2})
+            if i%500 is 0:
+                print(i,l)
+
+        for i in range(25):
+            l,_=session.run([loss,train],feed_dict={rate: 1e-4})
+            if i%500 is 0:
+                print(i,l)
+
+        saver.save(session,"log/last.ckpt")
+
+        session.run(v_init_op)
+        a,b=session.run([input_layer,out_rho])
+        #print(a,b)
+
+    figure()
+    e2,r2=dd.transpose()
+    plot(epsilon,rho,"-",alpha=0.5)
+    plot(e2,r2,',',alpha=0.1)
+    
+    plot(a,b)
+
+    xlabel(r"$\beta$")
+    ylabel(r"$\rho$")
+    show()
+
+if __name__=="__main__":
+    tf.app.run()
